b269da1ec249ee85592043c23b1754cdcb2232a4
Robert Postill <robert@grinning-cat.com>
2021-07-11 11:59:10 +1000

    fix priviledges and a minor syntax error

--- setup.sql
+++ setup.sql
@@ -1,11 +1,18 @@
 CREATE USER robert WITH PASSWORD 'bhujasample4$';
 CREATE ROLE dbowner nologin;
-GRANT dbowner TO robert, postgres
+GRANT dbowner TO robert, postgres;
 CREATE DATABASE pii;
 ALTER database pii OWNER TO dbowner;
 
 
 \connect pii;
+
+ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA public
+   GRANT ALL ON TABLES TO robert;
+ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA public
+   GRANT ALL ON SEQUENCES TO robert;
+ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA public
+   GRANT ALL ON FUNCTIONS TO robert;
 
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
