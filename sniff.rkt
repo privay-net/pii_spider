6c94501ffcef870ec9ee2babb6c2b7bd881534fe
Robert Postill <robert@grinning-cat.com>
2021-09-04 23:13:30 +1000

    Naive query of rows for examination

--- sniff.rkt
+++ sniff.rkt
@@ -16,17 +16,7 @@
   ; return pii rows
 
   ; return report
-  url
-  )
-
-(define (initialise-connection #:connector [postgresql-connect postgresql-connect])
-      (postgresql-connect #:user "robert"
-                        #:database "pii"
-                        #:password "bhujasample4$"))
-
-(define (examine-table connection table-name #:query-function [query-rows query-rows])
-  (let ([query (string-append "select * from \"" table-name "\";")])
-    (query-rows connection query)))
+  url)
 
 (module+ test
   (require rackunit)
@@ -48,15 +38,29 @@
   
   (test-case "sniffer compiles a list of tables to examine"
     (sniffer "not://a.url/test" #:connector connector-mock #:list-tables list-tables-mock)
-    (check-mock-called-with? list-tables-mock (arguments test-connection)))
+    (check-mock-called-with? list-tables-mock (arguments test-connection))))
 
+
+(define (initialise-connection #:connector [postgresql-connect postgresql-connect])
+  (postgresql-connect #:user "robert"
+                      #:database "pii"
+                      #:password "bhujasample4$"))
+
+(module+ test
   ;;; TODO deal with a failed connection
+  ;;;  TODO pool connections
   (test-case "initialise-connection gets the connection for a database"
     (initialise-connection #:connector connector-mock)
     (check-mock-called-with? connector-mock (arguments #:database "pii"
                                                        #:password "bhujasample4$"
-                                                       #:user "robert")))
+                                                       #:user "robert"))))
 
+
+(define (examine-table connection table-name #:query-function [query-rows query-rows])
+  (let ([query (string-append "select * from \"" table-name "\";")])
+    (query-rows connection query)))
+
+(module+ test
   ;;; TODO deal with table not existing error  
   (test-case "examine-table exectutes a query with the requiired arguments"
     (define query-mock (mock #:behavior (const test-connection)))
@@ -64,9 +68,28 @@
     (check-mock-called-with? query-mock (arguments connector-mock "select * from \"foo\";")))
   
   (test-case "examine-table returns a list of rows"
-    (define row-result '(#(1 "user@example.com")))
-    (define query-mock (mock #:behavior (const row-result)))
-    (check-eq? (examine-table connector-mock "foo" #:query-function query-mock) row-result)))
+    (define one-row-result '(#(1 "user@example.com")))
+    (define query-mock (mock #:behavior (const one-row-result)))
+    (check-eq? (examine-table connector-mock "foo" #:query-function query-mock) one-row-result)))
+
+
+(define (sniff-for-pii row rule)
+  (vector-map rule row))
+
+(module+ test
+  (define row-result #(1 "user@example.com"))
+  
+  (test-case "sniff-for-pii run rules over each row looking for PII"
+    (define-opaque test-rule)
+    (define rule-mock (mock #:behavior (const test-rule)))
+    (sniff-for-pii row-result rule-mock)
+    (check-mock-called-with? rule-mock (arguments (vector-ref row-result 0)))
+    (check-mock-called-with? rule-mock (arguments (vector-ref row-result 1))))
+
+  (test-case "sniff-for-pii returns a count of the PII instances detected")
+  (test-case "sniff-for-pii returns the name of the rule when PII is detected")
+  (test-case "sniff-for-pii returns an count of 0 when no PII is detected")
+  (test-case "sniff-for-pii returns the rule name when no  PII instances detected"))
 
 
 
