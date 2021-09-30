2978a5431bf2266597f2ad6e8bea7f7f4c16f001
Robert Postill <robert@grinning-cat.com>
2021-09-30 18:17:29 +1000

    Make email ignore non-string datatypes

--- pii/rules.rkt
+++ pii/rules.rkt
@@ -5,7 +5,9 @@
 ;; TODO have this maybe with levels of expense for deeper checking i.e. level 1 - regexp level 2 - domain check level 3 - test email
 (define (email candidate)
   (define simple-email-regex (pregexp "^\\S+@\\S+\\.\\S+$"))
-  (list "email address" (regexp-match? simple-email-regex candidate)))
+  (if (string? candidate)
+      (list "email address" (regexp-match? simple-email-regex candidate))
+      (list "email address" #f)))
 
 (module+ test
   (require rackunit
@@ -17,5 +19,7 @@
   (test-case "returns #t for an email address"
     (check-true (cadr (email "robert@test.com"))))
   (test-case "returns #f when not an email address"
-    (check-false (cadr (email "test")))))
+    (check-false (cadr (email "test"))))
+  (test-case "returns #f when not a string"
+    (check-false (cadr (email 1)))))
 
