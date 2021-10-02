6e051671d194fcc2b5991fe9f259305a24082186
Robert Postill <robert@grinning-cat.com>
2021-10-03 01:00:49 +1000

    Add phone number detection

--- pii/rules.rkt
+++ pii/rules.rkt
@@ -1,6 +1,6 @@
 #lang racket
 
-(provide email)
+(provide email au-phone-number)
 
 ;; TODO have this maybe with levels of expense for deeper checking i.e. level 1 - regexp level 2 - domain check level 3 - test email
 (define (email candidate)
@@ -13,7 +13,6 @@
   (require rackunit
            mock
            mock/rackunit)
-
   (test-case "returns the rule name"
     (check-equal? (car (email "test")) "email address"))
   (test-case "returns #t for an email address"
@@ -23,3 +22,23 @@
   (test-case "returns #f when not a string"
     (check-false (cadr (email 1)))))
 
+(define (au-phone-number candidate)
+  (define simple-regex #px"[+61|0]\\d{3}\\s?\\d{3}\\s?\\d{3}")
+  (if (string? candidate)
+      (list "AU phone number" (regexp-match? simple-regex candidate))
+      (list "AU phone number" #f)))
+
+(module+ test
+  (test-case "returns the rule name"
+    (check-equal? (car (au-phone-number "test")) "AU phone number"))
+  (test-case "returns #t for an AU phone number"
+    (check-true (cadr (au-phone-number "0412345678"))))
+  (test-case "returns #t for an AU phone number with country prefix"
+   (check-true (cadr (au-phone-number "+61412345678"))))
+  (test-case "returns #t for an AU phone number with spaces"
+    (check-true (cadr (au-phone-number "0412 345 678")))
+    (check-true (cadr (au-phone-number "0412 345678"))))
+  (test-case "returns #f when not an AU phone number"
+    (check-false (cadr (au-phone-number "test"))))
+  (test-case "returns #f when not a string"
+    (check-false (cadr (au-phone-number 1)))))
