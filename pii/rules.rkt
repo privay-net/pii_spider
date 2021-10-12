b765af8fec2b4f383f72382c9ac67e3bb5b439c8
Robert Postill <robert@grinning-cat.com>
2021-10-12 18:48:01 +1100

    Checked that the rules could be enforced inside larger strings

--- pii/rules.rkt
+++ pii/rules.rkt
@@ -4,7 +4,7 @@
 
 ;; TODO have this maybe with levels of expense for deeper checking i.e. level 1 - regexp level 2 - domain check level 3 - test email
 (define (email candidate)
-  (define simple-email-regex (pregexp "^\\S+@\\S+\\.\\S+$"))
+  (define simple-email-regex (pregexp "\\S+@\\S+\\.\\S+"))
   (if (string? candidate)
       (list "email address" (regexp-match? simple-email-regex candidate))
       (list "email address" #f)))
@@ -17,6 +17,8 @@
     (check-equal? (car (email "test")) "email address"))
   (test-case "returns #t for an email address"
     (check-true (cadr (email "robert@test.com"))))
+  (test-case "returns #t for an email address inside a string"
+    (check-true (cadr (email "the string is about robert@test.com being tested"))))
   (test-case "returns #f when not an email address"
     (check-false (cadr (email "test"))))
   (test-case "returns #f when not a string"
@@ -34,10 +36,13 @@
   (test-case "returns #t for an AU phone number"
     (check-true (cadr (au-phone-number "0412345678"))))
   (test-case "returns #t for an AU phone number with country prefix"
-   (check-true (cadr (au-phone-number "+61412345678"))))
+    (check-true (cadr (au-phone-number "+61412345678"))))
   (test-case "returns #t for an AU phone number with spaces"
     (check-true (cadr (au-phone-number "0412 345 678")))
     (check-true (cadr (au-phone-number "0412 345678"))))
+(test-case "returns #t for an AU phone number inside a larger string"
+    (check-true (cadr (au-phone-number "we should ring 0412 345 678")))
+    (check-true (cadr (au-phone-number "0412 345678 is a nice phone number")))) 
   (test-case "returns #f when not an AU phone number"
     (check-false (cadr (au-phone-number "test"))))
   (test-case "returns #f when not a string"
