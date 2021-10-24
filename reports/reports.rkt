93f0c644b904451b32f2e7d740813007b0a132d6
Robert Postill <robert@grinning-cat.com>
2021-10-25 00:20:53 +1100

    Begin to flesh out the report rows

--- reports/reports.rkt
+++ reports/reports.rkt
@@ -27,14 +27,32 @@
     (html-report test-rows #:table-creator table-creator-mock)
     (check-mock-called-with?  table-creator-mock (arguments test-rows))))
 
-(define (create-data-table rows)
+(define (create-data-table rows #:row-creator [create-data-table-rows create-data-table-rows])
   (txexpr* 'table empty
            (txexpr* 'tr empty
                     (txexpr 'th empty (list "Key"))
-                    (txexpr 'th empty (list "Rule")))))
+                    (txexpr 'th empty (list "Rule")))
+           (create-data-table-rows rows)))
 
 (module+ test
   (test-case "create-data-table will create a table presenting the result"
     (define result "<table><tr><th>Key</th><th>Rule</th></tr></table>")
     (define test-row (list 1 2 3 4))
-    (check-equal? (xexpr->html (create-data-table test-row)) result)))
+    (check-equal? (xexpr->html (create-data-table test-row)) result))
+  (test-case "create-data-table calls the row-creator"
+    (define test-rows '(1 2))
+    (define mock-result (txexpr 'tr empty (list "test")))
+    (define row-creator-mock (mock #:behavior (const mock-result)))
+    (create-data-table test-rows #:row-creator row-creator-mock)
+    (check-mock-called-with?  row-creator-mock (arguments test-rows))))
+
+(define (create-data-table-rows rows)
+  (txexpr* 'tr empty
+           (txexpr 'td empty (list "blah"))
+           (txexpr 'td empty (list "blah"))))
+
+(module+ test
+  (test-case "create-data-table-rows will create a set of rows"
+    (define result "<tr><td>Blah</td><td>Blah</td></tr>")
+    (define test-rows '(1 2))
+    (check-equal? (xexpr->html (create-data-table-rows test-rows)) result)))
