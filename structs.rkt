d62566185a5235ddc22a7ce51dbf557e6c30861e
Robert Postill <robert@grinning-cat.com>
2021-11-06 12:35:58 +1100

    add a structure for table metadata

--- structs.rkt
+++ structs.rkt
@@ -2,7 +2,7 @@
 
 (require racket/struct)
 
-(provide examined-row examined-row-id examined-row-results)
+(provide examined-row examined-row-id examined-row-results examined-table examined-table-name)
 
 (struct examined-row (id results)
   #:methods gen:custom-write
@@ -10,3 +10,12 @@
      (make-constructor-style-printer
       (lambda (obj) 'examined-row)
       (lambda (obj) (list (examined-row-id obj) (examined-row-results obj)))))]) 
+
+(struct examined-table (name start-time end-time row-count results)
+  #:methods gen:custom-write
+  [(define write-proc
+     (make-constructor-style-printer
+      (lambda (obj) 'examined-table)
+      (lambda (obj) (list (examined-table-name obj)
+                          (examined-table-row-count obj)
+                          (examined-table-end-time)))))]) 
