cb8c17ac57e9a7eae73a62ae5767d4d4de2aaed9
Robert Postill <robert@grinning-cat.com>
2021-12-12 11:20:19 +1100

    Implement AU TFN checking

--- pii/rules.rkt
+++ pii/rules.rkt
@@ -48,11 +48,21 @@
   (test-case "returns #f when not a string"
     (check-false (cadr (au-phone-number 1)))))
 
+;; Check this handy helper for more CC number formats
+;; https://en.wikipedia.org/wiki/Payment_card_number
+;; https://stackoverflow.com/questions/9315647/regex-credit-card-number-tests
+;; https://www.creditcardinsider.com/learn/anatomy-of-a-credit-card/
+;; https://www.paypalobjects.com/en_GB/vhelp/paypalmanager_help/credit_card_numbers.htm
+;; TODO handle number fields correctly
+;; TODO handle more card issuers
+;; TODO validate the card numbers more thoroughly
 (define (credit-card candidate)
   (define visa-mc-regex (pregexp "[452]\\d{3}[\\s-]?\\d{4}[\\s-]?\\d{4}[\\s-]?\\d{4}"))
   (define amex-regex (pregexp "3[47]\\d{2}[\\s-]?\\d{6}[\\s-]?\\d{5}"))
-  (list "Credit Card" (or (regexp-match? visa-mc-regex candidate)
-                          (regexp-match? amex-regex candidate))))
+  (if (string? candidate)
+      (list "Credit Card" (or (regexp-match? visa-mc-regex candidate)
+                              (regexp-match? amex-regex candidate)))
+      (list "Credit Card" #f)))
 
 (module+ test
   (test-case "returns the rule name"
@@ -73,3 +83,39 @@
     (check-true (cadr (credit-card "3712 388395 71772"))))
   (test-case "returns #f when not a credit card number"
     (check-false (cadr (credit-card "test")))))
+
+(define (au-tax-file-number candidate)
+  (if (string? candidate)
+      (list "AU Tax File Number" (validate-tfn candidate))
+      (list "AU Tax File Number" #f)))
+
+(module+ test
+  (test-case "returns the rule name"
+    (check-equal? (car (au-tax-file-number "test")) "AU Tax File Number"))
+  (test-case "returns #f when not a AU tax file number"
+    (check-false (cadr (au-tax-file-number "test"))))
+  (test-case "returns #t for a valid AU TFN"
+    (check-true (cadr (au-tax-file-number "123456782"))))
+  (test-case "returns #t for a valid AU TFN with spaces"
+    (check-true (cadr (au-tax-file-number "123 456 782")))))
+
+;; see https://www.clearwater.com.au/code/tfn for the procedure used to calculate this
+(define (validate-tfn candidate)
+  (define tfn-regex (pregexp "(\\d{3})\\s?(\\d{3})\\s?(\\d{3})"))
+  (define magic-weights '(1 4 3 7 5 8 6 9 10))
+  (if (regexp-match tfn-regex candidate)
+      (zero?
+       (remainder 
+        (foldr + 0  (map * magic-weights
+                         (map string->number
+                              (map string
+                                   (string->list
+                                    (foldr string-append ""
+                                           (cdr (regexp-match tfn-regex candidate)))))))) 11))
+      #f))
+
+(module+ test
+  (test-case "returns #t for a valid AU TFN"
+    (check-true (validate-tfn "123456782")))
+  (test-case "returns #f for an invalid AU TFN"
+    (check-false (validate-tfn "123456789"))))
