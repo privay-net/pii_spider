8e7335e6fa507570a934254b12334dcaa4a5a73f
Robert Postill <robert@grinning-cat.com>
2022-02-24 00:27:00 +1100

    Made the bottom table of results look better

--- reports/reports.rkt
+++ reports/reports.rkt
@@ -22,7 +22,7 @@
                                    (txexpr 'link '((href "https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css") (rel "stylesheet"))))
                           (txexpr* 'body empty
                                    (txexpr* 'div '((class "min-w-screen min-h-screen bg-gray-200 flex-col p-3 overflow-auto"))
-                                            (txexpr 'h1 '((class "text-center text-5xl font-extrabold")) (list (string-append "Results for table " (examined-table-name table-results))))
+                                            (txexpr 'h1 '((class "text-center text-5xl font-extrabold")) (list (string-append "Report for table " (examined-table-name table-results))))
                                             (results-summary table-results)
                                             (row-table (examined-table-results table-results))))))
   (string-append "<!DOCTYPE html>" (xexpr->html report)))
@@ -39,7 +39,7 @@
   (define test-zero-record-table (examined-table "no_rows" start-time end-time 0 test-no-rows))
   
   (test-case "html-table-report produces a HTML report of the run"
-    (define result "<!DOCTYPE html><html lang=\"en\" class=\"no-js\"><head><meta charset=\"UTF-8\"/><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"/><title>PII Spider Report</title><meta name=\"description\" content=\"Report on PII discovered in this database\"/><link href=\"https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css\" rel=\"stylesheet\"/></head><body><div class=\"min-w-screen min-h-screen bg-gray-200 flex-col p-3 overflow-auto\"><h1 class=\"text-center text-5xl font-extrabold\">Results for table no_rows</h1><p>mock summary</p><p>mock table</p></div></body></html>")
+    (define result "<!DOCTYPE html><html lang=\"en\" class=\"no-js\"><head><meta charset=\"UTF-8\"/><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"/><title>PII Spider Report</title><meta name=\"description\" content=\"Report on PII discovered in this database\"/><link href=\"https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css\" rel=\"stylesheet\"/></head><body><div class=\"min-w-screen min-h-screen bg-gray-200 flex-col p-3 overflow-auto\"><h1 class=\"text-center text-5xl font-extrabold\">Report for table no_rows</h1><p>mock summary</p><p>mock table</p></div></body></html>")
     (define table-creator-mock (mock #:behavior (const (txexpr 'p empty (list "mock table")))))
     (define summary-mock (mock #:behavior (const (txexpr 'p empty (list "mock summary")))))
     (check-equal? (html-table-report test-zero-record-table #:table-creator table-creator-mock #:summary-creator summary-mock) result))
@@ -48,12 +48,11 @@
     (html-table-report test-zero-record-table #:table-creator table-creator-mock)
     (check-mock-called-with?  table-creator-mock (arguments (examined-table-results test-zero-record-table))))
   (test-case "html-table-report produces a report for the table"
-    (define result "<!DOCTYPE html><html lang=\"en\" class=\"no-js\"><head><meta charset=\"UTF-8\"/><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"/><title>PII Spider Report</title><meta name=\"description\" content=\"Report on PII discovered in this database\"/><link href=\"https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css\" rel=\"stylesheet\"/></head><body><div class=\"min-w-screen min-h-screen bg-gray-200 flex-col p-3 overflow-auto\"><h1 class=\"text-center text-5xl font-extrabold\">Results for table two_rows</h1><div><h1>Results for table two_rows</h1><table><tr><td>Start Time:</td><td>1970-01-01 00:00:00 +1000</td></tr><tr><td>End Time:</td><td>2000-02-28 13:14:00 +1100</td></tr><tr><td>Rows Examined:</td><td>2</td></tr></table></div><table><caption>Detailed results breakdown</caption><thead><tr><th>Key</th><th>Rule</th></tr></thead><tbody><tr><td>1</td><td><ul class=\"rule-list\"><li>email address</li><li>AU phone number</li></ul></td></tr><tr><td>2</td><td><ul class=\"rule-list\"><li>email address</li><li>AU phone number</li></ul></td></tr></tbody></table></div></body></html>")
+    (define result "<!DOCTYPE html><html lang=\"en\" class=\"no-js\"><head><meta charset=\"UTF-8\"/><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"/><title>PII Spider Report</title><meta name=\"description\" content=\"Report on PII discovered in this database\"/><link href=\"https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css\" rel=\"stylesheet\"/></head><body><div class=\"min-w-screen min-h-screen bg-gray-200 flex-col p-3 overflow-auto\"><h1 class=\"text-center text-5xl font-extrabold\">Report for table two_rows</h1><div class=\"p-3 m-2 bg-white rounded-md\"><table><tr><td>Start Time:</td><td>1970-01-01 00:00:00 +1000</td></tr><tr><td>End Time:</td><td>2000-02-28 13:14:00 +1100</td></tr><tr><td>Rows Examined:</td><td>2</td></tr></table></div><div class=\"p-3 m-2 bg-white rounded-md\"><h2 class=\"text-3xl\">Results</h2><table class=\"table-auto w-full\"><thead><tr><th>Key</th><th>Rule</th></tr></thead><tbody><tr class=\"border-b-2 border-gray-300\"><td class=\"text-center\">1</td><td><ul><li class=\"float-left p-1 m-4 border-2 rounded-full\"><span class=\"p1\">email address</span></li><li class=\"float-left p-1 m-4 border-2 rounded-full\"><span class=\"p1\">AU phone number</span></li></ul></td></tr><tr class=\"border-b-2 border-gray-300\"><td class=\"text-center\">2</td><td><ul><li class=\"float-left p-1 m-4 border-2 rounded-full\"><span class=\"p1\">email address</span></li><li class=\"float-left p-1 m-4 border-2 rounded-full\"><span class=\"p1\">AU phone number</span></li></ul></td></tr></tbody></table></div></div></body></html>")
     (check-equal? (html-table-report test-two-record-table) result)))
 
 (define (results-summary results)
-  (txexpr* 'div empty
-           (txexpr 'h1 empty (list (string-append "Results for table " (examined-table-name results))))
+  (txexpr* 'div '((class "p-3 m-2 bg-white rounded-md"))
            (txexpr* 'table empty
                     (txexpr* 'tr empty
                              (txexpr 'td empty '("Start Time:"))
@@ -67,22 +66,23 @@
 
 (module+ test
   (test-case "results-summary will create a div summarising the results of the table"
-    (define result "<div><h1>Results for table no_rows</h1><table><tr><td>Start Time:</td><td>1970-01-01 00:00:00 +1000</td></tr><tr><td>End Time:</td><td>2000-02-28 13:14:00 +1100</td></tr><tr><td>Rows Examined:</td><td>0</td></tr></table></div>")
+    (define result "<div class=\"p-3 m-2 bg-white rounded-md\"><table><tr><td>Start Time:</td><td>1970-01-01 00:00:00 +1000</td></tr><tr><td>End Time:</td><td>2000-02-28 13:14:00 +1100</td></tr><tr><td>Rows Examined:</td><td>0</td></tr></table></div>")
     (check-equal? (xexpr->html (results-summary test-zero-record-table)) result)))
 
 (define (row-table rows #:row-creator [row-table-body row-table-body])
-  (txexpr* 'table empty
-           (txexpr 'caption empty '("Detailed results breakdown"))
-           (row-table-header)
-           (row-table-body rows)))
+  (txexpr* 'div '((class "p-3 m-2 bg-white rounded-md"))
+           (txexpr 'h2 '((class "text-3xl")) (list "Results"))
+           (txexpr* 'table '((class "table-auto w-full"))
+                    (row-table-header)
+                    (row-table-body rows))))
 
 (module+ test
   (test-case "row-table will create a table presenting an empty result"
-    (define result "<table><caption>Detailed results breakdown</caption><thead><tr><th>Key</th><th>Rule</th></tr></thead><tbody><tr><td>No rows were examined</td></tr></tbody></table>")
+    (define result "<div class=\"p-3 m-2 bg-white rounded-md\"><h2 class=\"text-3xl\">Results</h2><table class=\"table-auto w-full\"><thead><tr><th>Key</th><th>Rule</th></tr></thead><tbody><tr class=\"border-b-2 border-gray-300\"><td>No rows were examined</td></tr></tbody></table></div>")
     (define test-row empty)
     (check-equal? (xexpr->html (row-table test-row)) result))
   (test-case "row-table will create a table presenting the result"
-    (define result "<table><caption>Detailed results breakdown</caption><thead><tr><th>Key</th><th>Rule</th></tr></thead><tbody><tr><td>1</td><td><ul class=\"rule-list\"><li>email address</li><li>AU phone number</li></ul></td></tr><tr><td>2</td><td><ul class=\"rule-list\"><li>email address</li><li>AU phone number</li></ul></td></tr></tbody></table>")
+    (define result "<div class=\"p-3 m-2 bg-white rounded-md\"><h2 class=\"text-3xl\">Results</h2><table class=\"table-auto w-full\"><thead><tr><th>Key</th><th>Rule</th></tr></thead><tbody><tr class=\"border-b-2 border-gray-300\"><td class=\"text-center\">1</td><td><ul><li class=\"float-left p-1 m-4 border-2 rounded-full\"><span class=\"p1\">email address</span></li><li class=\"float-left p-1 m-4 border-2 rounded-full\"><span class=\"p1\">AU phone number</span></li></ul></td></tr><tr class=\"border-b-2 border-gray-300\"><td class=\"text-center\">2</td><td><ul><li class=\"float-left p-1 m-4 border-2 rounded-full\"><span class=\"p1\">email address</span></li><li class=\"float-left p-1 m-4 border-2 rounded-full\"><span class=\"p1\">AU phone number</span></li></ul></td></tr></tbody></table></div>")
     (define test-rows (list  (examined-row (hash "key" '(1))
                                            '((1 "email address") (1 "AU phone number")))
                              (examined-row (hash "key" '(2))
@@ -109,13 +109,12 @@
 
 (define (row-table-body rows)
   (if (empty? rows)
-
       (txexpr* 'tbody empty
-               (txexpr* 'tr empty
+               (txexpr* 'tr '((class "border-b-2 border-gray-300"))
                         (txexpr 'td empty '("No rows were examined"))))
       (cons 'tbody (map (lambda (row)
-                          (txexpr* 'tr empty
-                                   (txexpr 'td empty
+                          (txexpr* 'tr '((class "border-b-2 border-gray-300"))
+                                   (txexpr 'td '((class "text-center"))
                                            (list (key->string
                                                   (hash-ref (examined-row-id row) "key"))))
                                    (txexpr* 'td empty
@@ -123,33 +122,28 @@
 
 (module+ test
   (test-case "row-table-body will return a defult message if the row list is empty"
-    (define result '(tbody (tr (td "No rows were examined"))))
+    (define result '(tbody (tr ((class "border-b-2 border-gray-300")) (td "No rows were examined"))))
     (define no-rows empty)
     (check-equal? (row-table-body no-rows) result))
   (test-case "row-table-body will create a data row with a single examined-row"
-    (define result '(tbody (tr (td "1")
-                               (td (ul ((class "rule-list"))
-                                       (li "email address")
-                                       (li "AU phone number"))))))
+    (define result '(tbody (tr ((class "border-b-2 border-gray-300")) (td ((class "text-center")) "1") (td (ul (li ((class "float-left p-1 m-4 border-2 rounded-full")) (span ((class "p1")) "email address"))
+                                                                                                               (li ((class "float-left p-1 m-4 border-2 rounded-full")) (span ((class "p1")) "AU phone number")))))))
     (define test-row (list (examined-row (hash "key" '(1))
                                          '((1 "email address") (1 "AU phone number")))))
     (check-equal? (row-table-body test-row) result))
   (test-case "row-table-body will create a data row with a complex key"
-    (define result '(tbody (tr (td "1, 2, three")
-                               (td (ul ((class "rule-list"))
-                                       (li "email address")
-                                       (li "AU phone number"))))))
+    (define result '(tbody (tr ((class "border-b-2 border-gray-300")) (td ((class "text-center")) "1, 2, three")
+                               (td (ul (li ((class "float-left p-1 m-4 border-2 rounded-full")) (span ((class "p1")) "email address"))
+                                       (li ((class "float-left p-1 m-4 border-2 rounded-full")) (span ((class "p1")) "AU phone number")))))))
     (define test-row (list (examined-row (hash "key" '(1 2 "three"))
                                          '((1 "email address") (1 "AU phone number")))))
     (check-equal? (row-table-body test-row) result))
   (test-case "row-table-body will create multiple data rows with multiple examined-rows"
-    (define result  '(tbody
-                      (tr (td "1") (td (ul ((class "rule-list"))
-                                           (li "email address")
-                                           (li "AU phone number"))))
-                      (tr (td "2") (td (ul ((class "rule-list"))
-                                           (li "email address")
-                                           (li "AU phone number"))))))
+    (define result '(tbody
+                     (tr ((class "border-b-2 border-gray-300")) (td ((class "text-center")) "1") (td (ul (li ((class "float-left p-1 m-4 border-2 rounded-full")) (span ((class "p1")) "email address"))
+                                                                                                         (li ((class "float-left p-1 m-4 border-2 rounded-full")) (span ((class "p1")) "AU phone number")))))
+                     (tr ((class "border-b-2 border-gray-300")) (td ((class "text-center")) "2") (td (ul (li ((class "float-left p-1 m-4 border-2 rounded-full")) (span ((class "p1")) "email address"))
+                                                                                                         (li ((class "float-left p-1 m-4 border-2 rounded-full")) (span ((class "p1")) "AU phone number")))))))
     (define test-rows (list  (examined-row (hash "key" '(1))
                                            '((1 "email address") (1 "AU phone number")))
                              (examined-row (hash "key" '(2))
@@ -176,9 +170,9 @@
 (define (rule-list rules)
   (if (empty? rules)
       (txexpr 'p empty '("No rules to display."))
-      (txexpr 'ul '((class "rule-list"))
+      (txexpr 'ul empty
               (for/list ([rule rules])
-                (quasiquote (li (unquote (cadr rule))))))))
+                (quasiquote (li ((class "float-left p-1 m-4 border-2 rounded-full")) (span ((class "p1")) (unquote (cadr rule)))))))))
 
 (module+ test
   (test-case "rule-list will return a default message if the rule list is empty"
@@ -186,7 +180,7 @@
     (define no-rules empty)
     (check-equal? (xexpr->html (rule-list no-rules)) result))
   (test-case "rule-list will return an unordered list of each rule"
-    (define result "<ul class=\"rule-list\"><li>email address</li><li>AU phone number</li></ul>")
+    (define result "<ul><li class=\"float-left p-1 m-4 border-2 rounded-full\"><span class=\"p1\">email address</span></li><li class=\"float-left p-1 m-4 border-2 rounded-full\"><span class=\"p1\">AU phone number</span></li></ul>")
     (define two-rules '((1 "email address") (1 "AU phone number")))
     (check-equal? (xexpr->html (rule-list two-rules)) result)))
 
